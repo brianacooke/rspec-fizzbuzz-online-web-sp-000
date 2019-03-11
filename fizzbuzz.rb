4800c3047f8f22df9406181b2e29de8ba9aad9c0
Briana Cooke <br.cooke12@gmail.com>
2019-03-11 19:33:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-   puts "Fizz"
+   "Fizz"
 elsif int % 5 == 0
-  puts "Buzz"
+  "Buzz"
 elsif int % 3 == 0 && int % 5 == 0
-  puts "Fizzbuzz"
+  "Fizzbuzz"
 else
   puts nil
-
 end
 end
