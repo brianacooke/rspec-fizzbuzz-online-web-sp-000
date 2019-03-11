db665cafdbb43bd8f708deb5a27ab5e78424dd59
Briana Cooke <br.cooke12@gmail.com>
2019-03-11 19:05:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,18 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+      "Fizz"
 end
+end
+
+def fizzbuzz(int)
+  if int % 5 == 0
+    "Buzz"
+  end
+end
+
+def fizzbuzz(int)
+  if int % 3 == 0
+    if int % 5 == 0
+      "Fizzbuzz"
+    end
+  end
